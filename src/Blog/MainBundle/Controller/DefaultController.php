c2de96774aa05cd5618d642082e62ca7753a4a4f
Jaron <jaronjking@gmail.com>
2015-10-11 13:27:15 -0700

    Added post view on MainBundle.  Also created not found view for posts not found.  Added next and previous feature to bottom of post.

--- src/Blog/MainBundle/Controller/DefaultController.php
+++ src/Blog/MainBundle/Controller/DefaultController.php
@@ -8,6 +8,20 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository('BlogAdminBundle:Post')->findAll();
         return $this->render('BlogMainBundle:Default:index.html.twig');
+    }
+
+    public function postAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('BlogAdminBundle:Post')->find($id);
+        if (!$post) {
+            return $this->render('BlogMainBundle:Default:notFound.html.twig');
+        }
+        return $this->render('BlogMainBundle:Default:post.html.twig', array(
+            'post' => $post
+        ));
     }
 }
