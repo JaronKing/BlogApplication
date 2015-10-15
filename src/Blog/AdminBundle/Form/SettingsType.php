574ecc81d85aa984b741297aed3568cdb7cb582a
Jaron <jaronjking@gmail.com>
2015-10-15 11:05:04 -0700

    Settings on Admin Panel.

---
+++ src/Blog/AdminBundle/Form/SettingsType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Blog\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SettingsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('body')
+            ->add('metaName')
+            ->add('metaDescription')
+            ->add('pages')
+            ->add('adSpaceHorizontal')
+            ->add('adSpaceVertical')
+            ->add('github')
+            ->add('twitter')
+            ->add('facebook')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\AdminBundle\Entity\Settings'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'blog_adminbundle_settings';
+    }
+}
